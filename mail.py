598155000135ecf63f168127603f4337ab2cc2be
kd411 <urwithshanu@gmail.com>
2019-07-06 23:49:43 +0530

    Update mail.py
    Mail with attatchment

--- mail.py
+++ mail.py
@@ -7,15 +7,17 @@ mail=Mail(app)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'urwithshanu@gmail.com'
-
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route("/")
 def index():
-   msg = Message('Hello', sender = 'urwithshanu@gmail.com', recipients = ['urwithshanu@outlook.com'])
-   msg.body = "Hello Flask message sent from Flask-Mail"
+   msg = Message('AntHill Creations Registration', sender = 'urwithshanu@gmail.com', recipients = ['urwithshanu@outlook.com'])
+   msg.body = "Confirmation Link !!!!!"
+   with app.open_resource("brochure.pdf") as fp:
+      msg.attach("brochure.pdf", "brochure/pdf", fp.read())
    mail.send(msg)
    return "Sent"
 
